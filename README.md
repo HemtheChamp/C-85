c1c06042229df5eb142905b44a33300c38802c81
HemtheChamp <93389391+HemtheChamp@users.noreply.github.com>
2023-10-15 08:20:37 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# C-85
+# PRO-C85-boilerplate
+
---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBYdIDq5_jNgN53UsqU47gWGgI7UfEk75U",
+    authDomain: "storytellingapp-ca7ce.firebaseapp.com",
+    projectId: "storytellingapp-ca7ce",
+    storageBucket: "storytellingapp-ca7ce.appspot.com",
+    messagingSenderId: "907878704005",
+    appId: "1:907878704005:web:fb0073a06c31792edf5aec"
+  };
